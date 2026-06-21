feffa2661b09c0c6641af9024370ffd8877d2ba4
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-21 17:41:16 -0500

    recursive search
    added the ability to sear through a vector to find a number

--- recursion.cpp
+++ recursion.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void f(){
@@ -22,7 +23,33 @@ int count (int x){
         return x * factorial(x - 1);
     }
 
+    bool search_helper(const vector<int> v, int target, int index){
+        cout<< "We are searching "<<endl;
+        if(index == v.size()){
+            return false;
+        }
+        else if(v[index] == target){
+            return true;
+        }
+        else{
+            return search_helper(v, target, index + 1);
+        }
+
+    }
+
+    bool search(const vector<int>& v, int target){ //start from 0 search//
+        return search_helper(v, target, 0);
+    }
+
+    /*bool search(const vector<int> v, int target, int index){// specific search variable is the index
+
+    }*/
+
+
 int main(){
+    vector <int> v = {1,2,3,4,5,6,7,8,9,10};
+    cout << search(v, 5) <<endl;
+
 
     cout << factorial(5) <<endl;
 
