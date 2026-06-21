489ef24a743974de78af822d503920dc71558200
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-21 17:01:54 -0500

    Creating a basic recursion
    created a basic recursion loop

--- recursion.cpp
+++ recursion.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void f(){
+    cout << "hi" <<endl;
+    f();
+}
+
+int count (int x){
+    if(x ==0){
+        return 0;
+    }
+
+        return 1 + count(x - 1);
+    }
+
 int main(){
+
+    cout << count(10) <<endl;
 
     return 0;
 }
